ac307bc2e072faea00c434b43abcea5272627512
yoninazarathy <yoninazarathy@gmail.com>
2024-04-25 21:13:37 +1000

    main commands

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
 import click
 
-def hello():
-    print("Hello!")
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def agent():
+    print("agent")
+
+@click.command()
+def composer():
+    print("composer")
+
+@click.command()
+def status():
+    print("status")
+
+@click.command()
+def config():
+    print("config")
+
+cli.add_command(agent)
+cli.add_command(composer)
+cli.add_command(status)
+cli.add_command(config)
 
 if __name__ == '__main__':
-    hello()
+    cli()
